1f2561d50bab5b3d221e4f25dbedc2f82b76b662
ManjuPaliwal <116282082+ManjuPaliwal@users.noreply.github.com>
2022-11-06 15:26:34 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var bars = document.getElementById('bar');
+var nav = document.querySelector('nav');
+bars.addEventListener('click',function(){
+    nav.classList.toggle('toggle')
+})
+// var projects = document.getElementById('projects');
+// projects.addEventListener('click',func)
